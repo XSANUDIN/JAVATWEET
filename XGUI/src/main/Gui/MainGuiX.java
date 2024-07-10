a250fe8433c58b7895ddcf11f305b55a5d20bcc5
XsanUdin <122749128+XSANUDIN@users.noreply.github.com>
2024-07-10 18:18:58 +0700

    Add files via upload

---
+++ XGUI/src/main/Gui/MainGuiX.java
@@ -0,0 +1,18 @@
+//import javax.swing.*;
+//
+//public class MainGuiX extends JFrame{
+//    private JPanel BtPanel;
+//    private JPanel MainP;
+//    private JPanel BtP;
+//
+//    public MainGuiX(){
+//        setContentPane(MainP);
+//        setVisible(true);
+//        setSize(700,600);
+//        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//    }
+//
+//    public static void main(String[] args) {
+//        new MainGuiX();
+//    }
+//}Q
